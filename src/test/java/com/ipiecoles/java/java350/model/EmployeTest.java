b79745ad35df44941955c318baaba4e9d922feb0
Altod69 <kelvinfreeze@hotmail.com>
2019-03-15 09:09:37 +0100

    ajout test

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class EmployeTest {
 
@@ -62,5 +64,35 @@ public class EmployeTest {
 		//Then
 		//nbAnnee = 0
 		Assertions.assertThat(nbAnnee).isEqualTo(0);
+	}
+	
+	@ParameterizedTest
+	@CsvSource({
+		"1, 'M12345', 0, 1.0, 1700.0",
+		"1, 'T12345', 2, 1.0, 1200.0",
+		"2, 'T12345', 0, 1.0, 2300.0",
+	})
+	public void testGetPrimeAnnuelle(Integer performance, String matricule, Long nbYearsAnciennete, Double tempsPartiel, Double primeAnnuelle) {
+		//Given
+		/* 
+		 * 
+		 * PrimeAncienneté
+		 * Prime
+		 * Matricule
+		 * Performance
+		 * TempsPartiel
+		 * 
+		 */
+		Employe employe = new Employe();
+		employe.setPerformance(performance);
+		employe.setMatricule(matricule);
+		employe.setTempsPartiel(tempsPartiel);
+		employe.setDateEmbauche(LocalDate.now().minusYears(nbYearsAnciennete));
+		//When
+		
+		Double prime = employe.getPrimeAnnuelle();
+		//Then
+				
+		Assertions.assertThat(prime).isEqualTo(primeAnnuelle);
 	}
 }
